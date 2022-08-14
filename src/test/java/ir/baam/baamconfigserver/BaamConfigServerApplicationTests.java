5fc47377e13c0f2750575a34ce4899fde8fd569a
ha.moradi <hamedmoradi.mailsbox@gmail.com>
2022-08-14 09:56:08 +0430

    Initial commit

---
+++ src/test/java/ir/baam/baamconfigserver/BaamConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.baam.baamconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaamConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
